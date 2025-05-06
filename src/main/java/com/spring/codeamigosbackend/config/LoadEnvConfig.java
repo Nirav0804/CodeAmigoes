e789aa62a9c13d09e2c06a017e7e43316527bf94
suhani <1008suhaniparmar@gmail.com>
2025-05-06 12:32:10 +0530

    config module added

---
+++ src/main/java/com/spring/codeamigosbackend/config/LoadEnvConfig.java
@@ -0,0 +1,10 @@
+package com.spring.codeamigosbackend.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+public class LoadEnvConfig {
+    public static void load(){
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(),entry.getValue()));
+    }
+}
