4fb261841bf60692785109b1d2efb6f4ab231826
suhani <1008suhaniparmar@gmail.com>
2025-05-06 12:57:37 +0530

    main file updated

--- src/main/java/com/spring/codeamigosbackend/CodeAmigosBackendApplication.java
+++ src/main/java/com/spring/codeamigosbackend/CodeAmigosBackendApplication.java
@@ -1,5 +1,6 @@
 package com.spring.codeamigosbackend;
 
+import com.spring.codeamigosbackend.config.LoadEnvConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CodeAmigosBackendApplication {
 
 	public static void main(String[] args) {
+		LoadEnvConfig.load();
 		SpringApplication.run(CodeAmigosBackendApplication.class, args);
 	}
 
