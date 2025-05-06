32a02049dbc0adab3645968e6cf435fe4748c62c
Nirav0804 <niravpatel0804@gmail.com>
2025-05-06 10:35:37 +0530

    first commit

---
+++ src/main/java/com/spring/codeamigosbackend/CodeAmigosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.spring.codeamigosbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeAmigosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeAmigosBackendApplication.class, args);
+	}
+
+}
