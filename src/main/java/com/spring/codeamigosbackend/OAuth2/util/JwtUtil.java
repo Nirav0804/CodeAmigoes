74ccdb0a5a2dd769591c63283b9874b6c6f689a6
khushi703 <zalavadiyakhushi80@gmail.com>
2025-05-26 17:35:19 +0530

    resolved jwtutil problem

---
+++ src/main/java/com/spring/codeamigosbackend/OAuth2/util/JwtUtil.java
@@ -0,0 +1,50 @@
+package com.spring.codeamigosbackend.OAuth2.util;
+import io.github.cdimascio.dotenv.Dotenv;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+@Component
+public class JwtUtil {
+    private static Dotenv dotenv = Dotenv.load();
+    private static final String SECRET_KEY = dotenv.get("JWT_SECRET_KEY") ; // Store in env variable
+    private static final SecretKey KEY = Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+    private static final long EXPIRATION_TIME = 24 * 60 * 60 * 1000; // 24 hour
+
+    public static String generateToken(String id, String username, String email,String status) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("id", id);
+        claims.put("username", username);
+        claims.put("status",status);
+        claims.put("email", email);
+
+        return Jwts.builder()
+                .setSubject(id)
+                .addClaims(claims)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(KEY, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public static Claims validateToken(String token) {
+        return Jwts.parser()
+                .verifyWith(KEY)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+
+
+    public static String getUserIdFromToken(String token) {
+        return validateToken(token).getSubject();
+    }
+}
